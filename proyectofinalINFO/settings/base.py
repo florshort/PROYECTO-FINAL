40449aed9010ad6a845773dd1f5231dd2965e628
cumpacho <cumpacho@gmail.com>
2022-08-21 01:09:05 -0300

    favicon solucionado

--- proyectofinalINFO/settings/base.py
+++ proyectofinalINFO/settings/base.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-5lswx!kgcki*8^i!5=hzc+jk_ddkqyttnj@z!5)*!mox#o6v&4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
